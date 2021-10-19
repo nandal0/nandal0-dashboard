8740bacf1c8806b6d47e3e1053e0b5d3fd2327b2
nandal0 <sagarnandal52@gmail.com>
2021-10-20 02:38:31 +0530

    commit

--- src/components/SideIcon.js
+++ src/components/SideIcon.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import './SideIcon.css'
 
 const SideIcon = ({Icon,text}) => {
     return (
-        <div>
-        <Icon/>
-            {text}
+        <div className="sideicon">
+        <Icon style={{marginLeft:'30px'}}
+    />
+         <h7 className="ms-3">{text}</h7>
         </div>
     )
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import './Sidebar.css'
 import MenuIcon from '@mui/icons-material/Menu';
+import MenuBookOutlinedIcon from '@mui/icons-material/MenuBookOutlined';
+import LaptopWindowsIcon from '@mui/icons-material/LaptopWindows';
+import OfflineShareIcon from '@mui/icons-material/OfflineShare';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import WineBarIcon from '@mui/icons-material/WineBar';
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
+import DataUsageOutlinedIcon from '@mui/icons-material/DataUsageOutlined';
 import SideIcon from './SideIcon';
+import { AccessTimeFilledOutlined, Favorite, Home, Plus, Star } from '@mui/icons-material';
+import { AccessTime, RecentActors } from '@material-ui/icons';
 const Sidebar = () => {
     return (
         <div className="sidebar">
             
             
             <SideIcon Icon={MenuIcon} />
+            <SideIcon Icon={Home} text="Home" />
+            <SideIcon Icon={Star} text="Favourite" />
+            <SideIcon Icon={AccessTimeFilledOutlined} text="Recent" />
+            <SideIcon Icon={AddRoundedIcon} text="Create" />
+            <SideIcon Icon={DataUsageOutlinedIcon} text="DataSheets" />
+            <SideIcon Icon={WineBarIcon} text="Goals" />
+            <SideIcon Icon={AppRegistrationIcon} text="Apps" />
+            <SideIcon Icon={OfflineShareIcon} text="Shared with me" />
+            <SideIcon Icon={LaptopWindowsIcon} text="Discover" />
+            <SideIcon Icon={MenuBookOutlinedIcon} text="Learn" />
+
         </div>
     )
 }
